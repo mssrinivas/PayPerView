b70b699331af61e0229dea495e3a72a68927ae65
drashti93 <drustihitesh.thakkar@sjsu.edu>
2019-05-10 17:55:31 -0700

    add card endpoint

--- WebApp/BackEnd/src/routes/profile.js
+++ WebApp/BackEnd/src/routes/profile.js
@@ -4,13 +4,12 @@ var bodyparser = require('body-parser');
 var urlencodedParser = bodyparser.urlencoded({extended: false});
 var Users = require('../models/users');
 
-router.get('/api/getProfile/:user_id', urlencodedParser, function(req, res){
+router.get('/api/getProfile/:email', urlencodedParser, function(req, res){
     
     console.log("Get Profile API")
-    console.log(req.params.user_id);
-    var user_id = req.params.user_id;
-    Users.findOne({user_id: user_id},
-    {courses: 0, submissions: 0, messages: 0}, function(err,user){
+    console.log(req.params.email);
+    var user_id = req.params.email;
+    Users.findOne({email: email}, function(err,user){
         if (err) {
             res.code = "400";
             res.status(400); 
@@ -26,18 +25,17 @@ router.post('/api/setProfile/', urlencodedParser, function(req, res){
     
     //console.log("Set Profile API" + req.body.user_id +  req.body.email + req.body.phone_number + req.body.about_me +  req.body.city +  req.body.company +  req.body.school+  req.body.country+  req.body.hometown+  req.body.languages+  req.body.gender)
     console.log(req.body.phone_number)
-    var user_id = req.body.user_id;
+    var user_id = req.body.email;
 
     if(req.body.phone_number==null) {
         req.body.phone_number=0;
     }
     Users.update(
         {
-            user_id: user_id
+            email: email
         },
         { $set: 
             {
-                email: req.body.email,
                 phone_number: req.body.phone_number,
                 about_me: req.body.about_me,
                 city: req.body.city,
@@ -56,6 +54,21 @@ router.post('/api/setProfile/', urlencodedParser, function(req, res){
                 res.status(200).json({});
             }
     })
+})
+
+router.post('/api/addCard', urlencodedParser, function(req, res){
+   
+    var email = req.body.email;
+    var cardId = parseInt(req.body.cardId);
+    var code = parseInt(req.body.cardCode);
+    var expDate = req.body.expiration_date;
+    Users.update({
+        email: email
+    }, {$set: {cards: {
+        cardId: cardId,
+        cardCode: code,
+        expiration_date: expDate
+    }}})
 })
 
 module.exports = router;
